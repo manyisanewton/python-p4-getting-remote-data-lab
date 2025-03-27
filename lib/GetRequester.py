3e0c975ab3a7c13ed5def59c548dd61a5dfe3943
Newton Manyisa <manyisanewton26@gmail.com>
2025-03-27 23:38:15 +0300

    i have learned how to send request to remote api

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,35 @@
+
+# A tool that helps Python fetch data from websites.import requests
 import requests
+
+# A tool to turn website data into something we can read (like lists and dictionaries)
 import json
 
-class GetRequester:
 
+
+
+class GetRequester:
     def __init__(self, url):
         self.url = url
 
+
+
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)#It grabs the data and stores it in response
+        return response.content# returns the raw data
+
+#the function below turns the raw data into something we can read easily (a Python list or dictionary).
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+
+
+
+
+if __name__ == "__main__":
+    url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    requester = GetRequester(url)#It creates a GetRequester robot with a real website link.
+    data = requester.load_json()#It fetches and converts the data into a readable format
+    print(data)#It prints the data to the screen
+
